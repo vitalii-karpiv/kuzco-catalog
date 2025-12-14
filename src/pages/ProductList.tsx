55abc6a1bd4f3478cbfdb0b503cffc8cee3be50e
Vitalii Karpiv <vitalia.karpiv@gmail.com>
2025-12-14 19:25:29 +0100

    fix title

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -144,7 +144,7 @@ const ProductList = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
-              <h1 className="text-2xl font-bold text-white">Каталог ноутбуків Kuzco</h1>
+              <h1 className="text-2xl font-bold text-white">Каталог ноутбуків</h1>
             </div>
             
             {/* Mobile Filter Toggle */}
